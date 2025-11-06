b9692aa58caf77cf495f5874aaef04a3fd5f4022
toolsconstruix <tools.construix@gmail.com>
2025-11-06 16:42:14 -0300

    Fix: corrigir preload de imagem hero para funcionar em produção

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Preload critical image
+const heroImage = new Image();
+heroImage.src = new URL('./assets/hero-house.jpg', import.meta.url).href;
+
 createRoot(document.getElementById("root")!).render(<App />);
